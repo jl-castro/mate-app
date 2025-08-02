b532b9d2a8c056dae4f0c8f48fc4893a53f2b4ec
jorge.castro <jorgeluiscastrov5@gmail.com>
2025-08-02 15:38:23 -0400

    feature(timer): timer added

--- src/app/fractions/subtraction/subtraction-fractions.component.ts
+++ src/app/fractions/subtraction/subtraction-fractions.component.ts
@@ -1,12 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { QuizShellComponent } from '../../shared/quiz/quiz-shell';
 import { FractionQuiz } from '../../shared/models/quiz-data.interface';
 import { generateFractionSubtractionQuiz } from './fraction-subtraction.generator';
+import { Timer } from '../../shared/components/timer/timer';
+import { QuizTimerService } from '../../core/services/quiz-timer.service';
 
 @Component({
   standalone: true,
   selector: 'app-subtraction-fractions',
   template: `
+    <app-timer [duration]="60" (finished)="onTimeUp()"></app-timer>
+
     <app-quiz-shell
       [question]="data.question"
       [options]="data.options"
@@ -14,10 +18,11 @@ import { generateFractionSubtractionQuiz } from './fraction-subtraction.generato
       (onCorrect)="nextQuestion()"
     />
   `,
-  imports: [QuizShellComponent]
+  imports: [QuizShellComponent, Timer]
 })
 export class SubtractionFractionsComponent {
   public data: FractionQuiz;
+  private quizTimerService = inject(QuizTimerService);
 
   constructor() {
     this.data = this.generateQuestion();
@@ -31,5 +36,9 @@ export class SubtractionFractionsComponent {
 
   nextQuestion() {
     this.data = this.generateQuestion();
+  }
+
+  onTimeUp() {
+    this.quizTimerService.onTimeUp();
   }
 }
